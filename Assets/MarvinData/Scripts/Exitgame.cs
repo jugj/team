cffabe86756918bc3ae0d2f7a587eb1171b19f81
Marvin <jugj@users.noreply.github.com>
2022-08-06 10:38:42 +0200

    Änderungen von Marvin um 10:38:42

---
+++ Assets/MarvinData/Scripts/Exitgame.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exitgame : MonoBehaviour
+{
+    public void exit() {
+        Application.Quit();
+    }
+}
