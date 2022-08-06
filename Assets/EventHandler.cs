3bba9f646f8b2f02e7f8e31549feae29849bb254
Simon <jugj@users.noreply.github.com>
2022-08-06 11:09:55 +0200

    Änderungen von Simon um 11:09:55

--- Assets/EventHandler.cs
+++ Assets/EventHandler.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum Item
+{
+    None,
+    TimeSwaper,
+    TimeChipPast,
+    TimeChipPresent,
+    TimeChipFuture
+}
 public class EventHandler : MonoBehaviour
 {
-    private List<Animator> actionsAnimations;
+    public List<Animator> actionsAnimations;
+    public List<Item> items;
     public ActionEvent currentEvent;
     public void Interact()
     {
         if (currentEvent != ActionEvent.None)
            actionsAnimations[(int)currentEvent].SetBool("Happend", true);
+    }
+    public void GetItem(int item)
+    {
+        if (item == 1)
+        {
+
+        }
     }
     void Start()
     {
