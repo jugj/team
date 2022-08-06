2bec73c1de5927daef9b71753b025f3892889f76
Marina <jugj@users.noreply.github.com>
2022-08-06 18:21:37 +0200

    Änderungen von Marina um 18:21:37

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -10,11 +10,13 @@ public class Player : MonoBehaviour
     }
     float horizontal = 0f;
     float vertical = 0f;
+    
     void Update()
-    {
+     {
         if(Input.GetKey(KeyCode.W) | Input.GetKey(KeyCode.UpArrow))
         {
             vertical += 1f;
+            GetComponent<AudioSource>().Play(0);
         }
         if (Input.GetKey(KeyCode.S) | Input.GetKey(KeyCode.DownArrow))
         {
