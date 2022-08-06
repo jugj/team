702616792a9182e6d1ff3486f22fc40aad0b5608
Simon <jugj@users.noreply.github.com>
2022-08-06 13:06:01 +0200

    Änderungen von Simon um 13:06:01

--- Assets/TimeSwapUi.cs
+++ Assets/TimeSwapUi.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class TimeSwapUi : MonoBehaviour
 {
+    private float startMouseDownDirection;
+    public Transform directionIndicator;
+    //private Vector2 endMouseDownDirection;
     void Start()
     {
         
     }
     void Update()
     {
-        //if ()
-        //{
-
-        //}
+        directionIndicator.up = Camera.main.ScreenToWorldPoint(Input.mousePosition) - directionIndicator.transform.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            startMouseDownDirection = transform.localEulerAngles.z;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, /*startMouseDownDirection +*/ directionIndicator.rotation.eulerAngles.z);
+        }
     }
 }
